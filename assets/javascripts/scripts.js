f0bc6d8aaee7611cfd974b305c2393ae5a27efde
Michael Collins <mscoll85@gmail.com>
2018-09-24 14:33:06 -0400

    updates

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -6,54 +6,6 @@
 // Init Foundation
 $(document).foundation();
 
-// Init Highlight.js
-//hljs.initHighlightingOnLoad();
-
-
-// Sticky Header
-// $('.top-bar').on('sticky.zf.stuckto:top', function(){
-//   $(this).addClass('shrink');
-// }).on('sticky.zf.unstuckfrom:top', function(){
-//   $(this).removeClass('shrink');
-// })
-
-
-
-
-// $(window).load(function(){
-	
-	
-	// var tabID = $("#pathways-panel div.tabs-panel").attr('id');
-
- //    $('#pathways-panel [data-tabs] li').on('click', function(e) {
- //    	var activeID = $(this).attr("id");
- //        console.log(activeID);
- //        $('div[id="' + activeID + '"]').addClass( 'is-active' );
- //        e.preventDefault();
-	// });
-
-	// if (tabID == activeID) {
-		
-	// 	$("#pathways-panel li.tabs-title").click(function(){
- //    	  $("#pathways-panel div.tabs-panel").addClass("is-active");
- // 		});
-
-		
-	// }
-
-
-	// if(!!tabActive){
-	// 	$(tabActive).find("a").attr('href');
-	// 	var hrefID = $(this);
-
-	// 	if (tabID == hrefID) {
-	// 		$(".tabs-panel").addClass("is-active");
-	// 	}
-		
-	// }
-// });
-
-
 (function($){
 
     var $doc = $(document),
@@ -62,6 +14,8 @@ $(document).foundation();
     $win.on('load', function(){
         // document is fully loaded
 
+
+        
         $( "#pathway-accordion li.accordion-group-tab, #curriculum-accordion li.accordion-group-tab" ).click(function(){
 
             var $container = $('.courseContainer');
@@ -88,5 +42,3 @@ $(document).foundation();
     });
     
 })(jQuery);
-
-
